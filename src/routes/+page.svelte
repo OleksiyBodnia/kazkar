a18930e02e19d68e182c3d253fcd90f8ea1cd8f7
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-03-01 15:42:59 +0200

    finished basic style for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,33 +2,43 @@
     import Kazka from '../components/Kazka.svelte';
 </script>
 
-<!-- <h1>Ласкаво просимо до Казкаря</h1> -->
-
-<div class="write-encourage">
-    <button>Написати казку</button>
-    <p>Створюй нову неймовірну казку разом із Казкарем!</p>
+<div class="main-page-div">
+    <div class="write-encourage">
+        <button>Написати казку</button>
+        <p>Створюй нову неймовірну казку разом із Казкарем!</p>
+    </div>
+    
+    <div class="read-finished">
+        <h1>Читай завершені казки</h1>
+            <div class="finished-samples">
+                <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
+                    Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
+                    Це поле соняшників ....</p>
+                <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
+                    Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
+                    Це поле соняшників ....</p>
+                <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
+                    Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
+                    Це поле соняшників ....</p>
+                <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
+                    Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
+                    Це поле соняшників ....</p>
+            </div>
+        <button>Більше</button>
+    </div>
 </div>
 
-<div class="read-finished">
-    <h1>Читай завершені казки</h1>
-        <div class="finished-samples">
-            <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
-                Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
-                Це поле соняшників ....</p>
-            <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
-                Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
-                Це поле соняшників ....</p>
-            <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
-                Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
-                Це поле соняшників ....</p>
-            <p>За дев’ятьма селами і дев’ятьма лісами є одне особливе поле. 
-                Там ростуть красиві, жовті квіти, які можуть повертатися за сонцем. 
-                Це поле соняшників ....</p>
-        </div>
-    <button>Більше</button>
-</div>
 
 <style>
+
+    .main-page-div{
+        padding: 40px 0;
+    }
+
+    .write-encourage{
+        margin: 0 0 20px 0;
+    }
+
     .write-encourage,.read-finished {
         text-align: center;
     }
@@ -38,18 +48,36 @@
     }
 
     .read-finished {
+        margin-top: 40px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
     }
 
     .finished-samples {
         width: 80%;
-        display: grid;
-        grid-template-columns: auto auto; /* Two columns */
-        grid-gap: 20px; /* Gap between grid items */
+        /* display: grid;
+        grid-template-columns: auto auto;
+        /* grid-gap: 20px; */
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        row-gap: 40px;
+        column-gap: 90px;
+        flex-wrap: wrap;
+        margin-bottom: 40px;
+
     }
+    .finished-samples p{
+        width: 500px;
+    }
+
 
     .finished-samples :hover {
         border-radius: 20px;
-        box-shadow: 0 15px 30px rgba(0, 0, 0, 0.3);
+        box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
         transition: all 0.5s ease;
     }
 </style>
