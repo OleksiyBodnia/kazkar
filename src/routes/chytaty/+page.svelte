8a317d359df78911eac2c203994c44f80dcf66bb
Лавриненко Богдан <bohdan_la@knu.ua>
2024-03-26 09:56:51 +0200

    maisternia and chytatu now more modular with new spare component

--- src/routes/chytaty/+page.svelte
+++ src/routes/chytaty/+page.svelte
@@ -3,95 +3,22 @@
 	export let data;
 	import KazkaCard from '../../components/KazkaCard.svelte';
 	import { combineRechennia } from '$lib/utils';
+	import ReadWriteTemplate from '../../components/ReadWriteTemplate.svelte';
 
-	let filter_selected;
-	let filters = ['Найновіші', 'Найстаріші', 'Популярні', 'Непопулярні'];
-
-	function whenFilterSelected() {}
 </script>
 
-<div class="read-page-div">
-	<div class="page-description">
-		<h1>Бібліотека завершених казкок</h1>
-		<p>
-			Тут ви можете знайти безліч завершених казок - читайте, досліджуйте, надихайтеся, смійтеся
-			(крінж опис треба поміняти)
-		</p>
-	</div>
-
-	<div class="find-tools">
-		<div class="rigth-find-tools">
-			<label
-				>Пошук
-				<input type="text" />
-			</label>
-
-			<label
-				>Фільтр
-				<select bind:value={filter_selected} on:change={whenFilterSelected}>
-					{#each filters as filter}
-						<option value={filter}>
-							{filter}
-						</option>
-					{/each}
-				</select>
-			</label>
+<ReadWriteTemplate
+	header={"Бібліотека завершених казок"}
+	description={"Тут ви можете знайти безліч завершених казок - читайте, досліджуйте, \
+					надихайтеся, смійтеся (крінж опис треба поміняти)"}
+>
+	{#each data.kazky as kazka}
+		<div class="sample">
+			<KazkaCard id={kazka.id} title={kazka.title} content={combineRechennia(kazka.rechennia)} />
 		</div>
-		<button>Випадкова казка</button>
-	</div>
-
-	<div class="finished-samples">
-		{#each data.kazky as kazka}
-			<div class="sample">
-				<KazkaCard id={kazka.id} title={kazka.title} content={combineRechennia(kazka.rechennia)} />
-			</div>
-		{/each}
-	</div>
-</div>
+	{/each}
+</ReadWriteTemplate>
 
 <style>
-	.read-page-div {
-		padding: 40px 0;
-	}
 
-	.page-description {
-		text-align: center;
-	}
-
-	.finished-samples {
-		margin: 0 auto;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		row-gap: 10px;
-		column-gap: 90px;
-		flex-wrap: wrap;
-		margin-bottom: 40px;
-	}
-
-	.find-tools {
-		padding: 0 30px;
-		margin-top: 30px;
-		margin-bottom: 30px;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-	}
-	.rigth-find-tools {
-		display: flex;
-		align-items: center;
-		gap: 40px;
-	}
-
-	/* на майбутнє: треба гатрно силізувати input, поки не шарю як */
-	input {
-		border: none;
-		border-bottom: 1px solid black;
-		background-color: transparent;
-		outline: none;
-	}
-
-	input:focus {
-		border-bottom: 2px solid black;
-	}
 </style>
