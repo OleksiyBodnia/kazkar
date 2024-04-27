4fc8de7d3682d7f8f391cc68d0b69fb48e0112a5
Oleksandr Brazhii <brazhiyolexandr@gmail.com>
2024-04-27 12:00:43 +0300

    little nothing

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,8 @@
 <script>
-	import InfoDialog from "./InfoDialog.svelte";
-	import LoginDialog from "./LoginDialog.svelte";
-	import { signIn, signOut} from "@auth/sveltekit/client";
-	import { page } from "$app/stores";
-	import {createDBUser} from "$lib/db/index.js";
-
+	import InfoDialog from './InfoDialog.svelte';
+	import LoginDialog from './LoginDialog.svelte';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
 
 	let InfoDialogComponent;
 	let LoginDialogComponent;
@@ -20,15 +18,14 @@
 	}
 
 	 */
-
-
 </script>
 
 <InfoDialog bind:this={InfoDialogComponent} />
 <LoginDialog bind:this={LoginDialogComponent} />
 
 <header>
-	<a class="title" href="/">Казкар</a> <!-- треба зробит так, щоб при натисканні головна строрінка оновлювалася навіть коли вона є поточною -->
+	<a class="title" href="/">Казкар</a>
+	<!-- треба зробит так, щоб при натисканні головна строрінка оновлювалася навіть коли вона є поточною -->
 	<nav>
 		<a href="/chytaty" class="header-link">Читати</a>
 		<a href="/maisternia" class="header-link">Писати</a>
@@ -36,8 +33,7 @@
 		<button on:click={InfoDialogComponent.toggleInfoDialog}>Що тут коїться?</button>
 		<!-- <button on:click={LoginDialogComponent.toggleLoginDialog}>Увійти</button> -->
 		{#if $page.data.session}
-			<button on:click={() => createDBUser()}>Тест</button>
-        	<button on:click={() => signOut()}>Вийти</button>
+			<button on:click={() => signOut()}>Вийти</button>
 		{:else}
 			<button on:click={() => signIn()}>Увійти</button>
 		{/if}
@@ -65,19 +61,20 @@
 		text-decoration: none;
 	}
 
-	nav a,nav button {
+	nav a,
+	nav button {
 		margin-left: 1em;
 	}
 
 	nav button {
-        border: none;
-        cursor: pointer;
-        padding: 0;
-        font: inherit;
-        outline: inherit;
+		border: none;
+		cursor: pointer;
+		padding: 0;
+		font: inherit;
+		outline: inherit;
 	}
 	.header-link:hover,
-	.header-link:focus{
+	.header-link:focus {
 		background-image: linear-gradient(120deg, #78009d 34%, #0087bc 100%);
 		background-clip: text;
 		-webkit-background-clip: text;
