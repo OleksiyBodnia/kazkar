de0991b11e8f55e98aad113d09ec721edeb90220
Лавриненко Богдан <bohdan_la@knu.ua>
2024-05-07 11:07:48 +0300

    user rech highlight

--- src/components/KazkaCard.svelte
+++ src/components/KazkaCard.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { truncateText, lastRechennia, combineRechennia } from '$lib/utils';
 	import WriteKazkaDialog from './WriteKazkaDialog.svelte';
+	import { page } from '$app/stores';
 	
 	export let kazka;
 	export let state;
@@ -11,24 +12,38 @@
 {#if state == "completed"}
 	<article>
 		<a href="/kazka/{kazka.id}">
-			<h4>{kazka.title}</h4>
-			<p>{truncateText(combineRechennia(kazka.rechennia), 160)}</p>
+			<div class="kazka-container">
+				<h4>{kazka.title}</h4>
+				{#each kazka.rechennia as rechennia}
+					{#if $page.data.session.user.id == rechennia.user_id}
+						<span style="color: var(--color-user);">{rechennia.content} </span>
+					{:else}
+						<span>{rechennia.content} </span>
+					{/if}
+					<span>{rechennia.content} </span>
+				{/each}
+			</div>
 		</a>
 	</article>
 {:else if state == "incompleted"}
 	<article on:click={() => WriteDialogComponent.toggleWrite()}>
-		<h4>{kazka.title}</h4>
-		<p>{truncateText(lastRechennia(kazka.rechennia).content, 160)}</p>
+		<div class="rech-conteiner">
+			<h4>{kazka.title}</h4>
+			<span>{lastRechennia(kazka.rechennia).content}</span>
+		</div>
 	</article>
 	<WriteKazkaDialog bind:this={WriteDialogComponent} type={"present"} {kazka} />
 {/if}
 
 <style>
 	article {
-		width: 500px;
+		width: 500px;	
 		/* some padding should be here */
 		border-radius: 24px;
-		padding: 1px 10px 10px 10px;
+		padding: 0px 10px 17px 10px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
 	}
 
 	article:hover {
@@ -41,7 +56,31 @@
 		text-align: center;
 	}
 
-	p {
+	span {
 		text-align: start;
+	}
+
+	/* button {
+		width: 100%;
+		all: unset;
+		
+	}
+
+	button:hover {
+		all: unset;
+	} */
+
+	.kazka-container {
+		height: 133px;
+		overflow: hidden;
+	}
+
+	.rech-conteiner {
+		height: 100px;
+		overflow: hidden;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		/* justify-content: center; */
 	}
 </style>
