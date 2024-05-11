d580f442ec906cd3b872dca57665b8687eabefd7
Лавриненко Богдан <bohdan_la@knu.ua>
2024-05-11 11:24:56 +0300

    fix auth errors

--- src/components/KazkaCard.svelte
+++ src/components/KazkaCard.svelte
@@ -15,8 +15,12 @@
 			<div class="kazka-container">
 				<h4>{kazka.title}</h4>
 				{#each kazka.rechennia as rechennia}
-					{#if $page.data.session.user.id == rechennia.user_id}
-						<span style="color: var(--color-user);">{rechennia.content} </span>
+					{#if $page.data.session}
+						{#if $page.data.session.user.id == rechennia.user_id}
+							<span style="color: var(--color-user);">{rechennia.content} </span>
+						{:else}
+							<span>{rechennia.content} </span>
+						{/if}
 					{:else}
 						<span>{rechennia.content} </span>
 					{/if}
