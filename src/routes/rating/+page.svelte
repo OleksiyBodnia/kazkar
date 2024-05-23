2c93167bdbff5759305ee2d40911bda8a35c8290
Oleksandr Brazhii <brazhiyolexandr@gmail.com>
2024-05-23 12:59:52 +0300

    Сторінку Рейтинг теж прискорено

--- src/routes/rating/+page.svelte
+++ src/routes/rating/+page.svelte
@@ -4,34 +4,61 @@
 	import { findLargestAttribute, transformDistribution } from '$lib/utils';
 
 	export let data;
-	data.distribution = transformDistribution(data.distribution);
 
 	let top_users = true;
 
 	let bars_scale = 120;
-	let fin_bar_multiplier = bars_scale / ( (data.finished > data.unfinished) ? data.finished : data.unfinished );
-	let top_bar_multiplier = bars_scale / (data.best_users[0].rech_count);
-	// let worst_bar_multiplier = bars_scale / (data.worst_users[4].rech_count);
-	let distrb_bar_multiplier = bars_scale / findLargestAttribute(data.distribution);
 
+	let fin_bar_multiplier;
+
+	let finished;
+	let unfinished;
+	Promise.all([data.finished, data.unfinished]).then((values) => {
+		finished = values[0];
+		unfinished = values[1];
+		fin_bar_multiplier = bars_scale / (finished > unfinished ? finished : unfinished);
+	});
+
+	let best_users;
+	let worst_users;
+	let top_bar_multiplier;
+	Promise.all([data.best_users, data.worst_users]).then((values) => {
+		best_users = values[0];
+		worst_users = values[1];
+		top_bar_multiplier = bars_scale / best_users[0].rech_count;
+	});
+
+	let distribution;
+	let distrb_bar_multiplier;
+	data.distribution.then((distr) => {
+		distribution = transformDistribution(distr);
+		distrb_bar_multiplier = bars_scale / findLargestAttribute(distribution);
+	});
 </script>
 
 <div>
 	<div class="rating-main-section">
 		<div class="site-statistics">
 			<h1>Статистика сайту</h1>
-			<div class="site-stat-bars">
-				<div class="finished-bar">
-					<label for="" >Завершених казок</label>
-					<div class="prog-finished-bar" style="width: {data.finished * fin_bar_multiplier}px;"></div>
-					<label for="">{data.finished}</label>
+			{#if !finished || !unfinished}
+				<p>Завантаження...</p>
+			{:else}
+				<div class="site-stat-bars">
+					<div class="finished-bar">
+						<label for="">Завершених казок</label>
+						<div class="prog-finished-bar" style="width: {finished * fin_bar_multiplier}px;"></div>
+						<label for="">{finished}</label>
+					</div>
+					<div class="unfinished-bar">
+						<label for="">Незавершених казок</label>
+						<div
+							class="prog-unfinished-bar"
+							style="width: {unfinished * fin_bar_multiplier}px;"
+						></div>
+						<label for="">{unfinished}</label>
+					</div>
 				</div>
-				<div class="unfinished-bar">
-					<label for="">Незавершених казок</label>
-					<div class="prog-unfinished-bar" style="width: {data.unfinished * fin_bar_multiplier}px;"></div>
-					<label for="">{data.unfinished}</label>
-				</div>
-			</div>
+			{/if}
 		</div>
 
 		<div class="users-statistics">
@@ -51,54 +78,77 @@
 				>
 			</div>
 
-			{#if top_users == true}
-				<div class="top-transitions" in:fade={{ delay: 100, duration: 1000 }}>
-					<p>Найактивніший казкар за кількістю написаних речень!</p>
-					<div class="best-kazkar">
-						<img src={data.best_users[0].image} alt="kazkar" class="best-kazkar-img" />
-						<p>{data.best_users[0].name}</p>
+			{#await Promise.all([data.best_users, data.worst_users])}
+				<p>Завантаження...</p>
+			{:then}
+				{#if top_users == true}
+					<div class="top-transitions" in:fade={{ delay: 100, duration: 1000 }}>
+						<p>Найактивніший казкар за кількістю написаних речень!</p>
+						<div class="best-kazkar">
+							<img src={best_users[0].image} alt="kazkar" class="best-kazkar-img" />
+							<p>{best_users[0].name}</p>
+						</div>
+						<div class="user-stat-bars">
+							{#each best_users as user}
+								<div class="user-bar">
+									<label for="">{user.name}</label>
+									<div
+										class="user-prog-bar"
+										style="width: {user.rech_count * top_bar_multiplier}px;"
+									></div>
+									<label for="">{user.rech_count}</label>
+								</div>
+							{/each}
+						</div>
 					</div>
-					<div class="user-stat-bars">
-						{#each data.best_users as user}
-							<div class="user-bar">
-								<label for="">{user.name}</label>
-								<div class="user-prog-bar" style="width: {user.rech_count * top_bar_multiplier}px;"></div>
-								<label for="">{user.rech_count}</label>
-							</div>
-						{/each}
+				{:else if top_users == false}
+					<div class="top-transitions" in:fade={{ delay: 100, duration: 1000 }}>
+						<p>Найменш активний казкар за кількістю написаних речень &#128542</p>
+						<div class="best-kazkar">
+							{#if worst_users[0].image}
+								<img src={worst_users[0].image} alt="kazkar" class="best-kazkar-img" />
+							{/if}
+							<p>{worst_users[0].name}</p>
+						</div>
+						<div class="user-stat-bars">
+							{#each worst_users as user}
+								<div class="user-bar">
+									<label for="">{user.name}</label>
+									<div
+										class="user-prog-bar"
+										style="width: {user.rech_count * top_bar_multiplier}px;"
+									></div>
+									<label for="">{user.rech_count}</label>
+								</div>
+							{/each}
+						</div>
 					</div>
-				</div>
-			{:else if top_users == false}
-				<div class="top-transitions" in:fade={{ delay: 100, duration: 1000 }}>
-					<p>Найменш активний казкар за кількістю написаних речень &#128542</p>
-					<div class="best-kazkar">
-						<img src={data.worst_users[0].image} alt="kazkar" class="best-kazkar-img" />
-						<p>{data.worst_users[0].name}</p>
-					</div>
-					<div class="user-stat-bars">
-						{#each data.worst_users as user}
-							<div class="user-bar">
-								<label for="">{user.name}</label>
-								<div class="user-prog-bar" style="width: {user.rech_count * top_bar_multiplier}px;"></div>
-								<label for="">{user.rech_count}</label>
-							</div>
-						{/each}
-					</div>
-				</div>
-			{/if}
+				{/if}
+			{:catch error}
+				<p>{error.message}</p>
+			{/await}
 		</div>
 
 		<div class="kazky-distribution-with-title">
 			<h1>Розподіл завершених казок</h1>
 			<span style="position: relative;">кількість казок</span>
 			<div class="kazky-distribution">
-				{#each Object.entries(data.distribution) as [key, value]}
-					<div class="kazka-distribution-bar">
-						<label for="" style="font-weight: 100;">{value}</label>
-						<div class="distribution-bar" style="width: 20px; height: {value * distrb_bar_multiplier}px;"><br /></div>
-						<label for="">{key}</label>
-					</div>
-				{/each}
+				{#await data.distribution}
+					<p>Завантаження...</p>
+				{:then}
+					{#each Object.entries(distribution) as [key, value]}
+						<div class="kazka-distribution-bar">
+							<label for="" style="font-weight: 100;">{value}</label>
+							<div
+								class="distribution-bar"
+								style="width: 20px; height: {value * distrb_bar_multiplier}px;"
+							>
+								<br />
+							</div>
+							<label for="">{key}</label>
+						</div>
+					{/each}
+				{/await}
 			</div>
 			<span style="position: relative;">кількість речень</span>
 		</div>
@@ -119,7 +169,7 @@
 		text-align: center;
 	}
 
-	p{
+	p {
 		text-align: center;
 	}
 
@@ -255,26 +305,27 @@
 		justify-content: center;
 		flex-direction: column;
 	}
-	@media screen and (max-width: 767px){
-		.rating-main-section{
+	@media screen and (max-width: 767px) {
+		.rating-main-section {
 			flex-direction: column;
 		}
-		.finished-bar, .unfinished-bar{
+		.finished-bar,
+		.unfinished-bar {
 			width: 350px;
 			justify-content: center;
 		}
-		.rating-main-section{
+		.rating-main-section {
 			gap: 40px;
 			margin-top: 4%;
 		}
-		.user-bar{
+		.user-bar {
 			width: 350px;
 			justify-content: center;
 		}
-		.kazky-distribution{
+		.kazky-distribution {
 			gap: 20px;
 		}
-		span{
+		span {
 			text-align: center;
 			width: 350px;
 		}
@@ -282,25 +333,25 @@
 			width: 350px;
 		}
 	}
-	@media screen and (min-width:768px) and (max-width: 1023px){
-		.rating-main-section{
+	@media screen and (min-width: 768px) and (max-width: 1023px) {
+		.rating-main-section {
 			flex-direction: column;
 			margin-top: 5%;
 		}
-		.finished-bar, .unfinished-bar{
+		.finished-bar,
+		.unfinished-bar {
 			width: 500px;
 			justify-content: center;
 		}
-		.user-bar{
+		.user-bar {
 			width: 500px;
 			justify-content: center;
 		}
-		h1{
+		h1 {
 			width: 500px;
 		}
-		.rating-main-section{
+		.rating-main-section {
 			gap: 60px;
 		}
-		
 	}
 </style>
