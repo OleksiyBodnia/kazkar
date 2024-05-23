67dd2cdd47a40327b028a7209d204b12ad058b61
Oleksandr Brazhii <brazhiyolexandr@gmail.com>
2024-05-23 12:59:52 +0300

    Сторінку Рейтинг теж прискорено

--- src/routes/rating/+page.server.js
+++ src/routes/rating/+page.server.js
@@ -2,11 +2,11 @@ import { getKazkyCount, getTopUsers, getKazkyDistribution} from '$lib/db';
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    const finished = await getKazkyCount('completed');
-    const unfinished = await getKazkyCount('incompleted');
-    const best_users = await getTopUsers(5);
-    const worst_users = await getTopUsers(5, false);
-    const distribution = await getKazkyDistribution();
+    const finished = getKazkyCount('completed');
+    const unfinished = getKazkyCount('incompleted');
+    const best_users = getTopUsers(5);
+    const worst_users = getTopUsers(5, false);
+    const distribution = getKazkyDistribution();
 
     return { finished, unfinished, distribution, best_users, worst_users };
 };
