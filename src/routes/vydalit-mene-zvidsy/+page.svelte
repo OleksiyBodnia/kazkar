5edf9af1c52ca10987b33111361701a28470300d
Oleksandr Brazhii <brazhiyolexandr@gmail.com>
2024-05-24 12:43:27 +0300

    Додано дві сторінки, які вимагає Facebook

---
+++ src/routes/vydalit-mene-zvidsy/+page.svelte
@@ -0,0 +1,14 @@
+<iframe
+	src="https://docs.google.com/forms/d/e/1FAIpQLSfV0kit2IKPElXA4x243fxjUUrhhR5t_Az8mOC-zVh-wa1vZQ/viewform?embedded=true"
+	frameborder="0"
+	marginheight="0"
+	marginwidth="0"
+	title="Embedded Form">Завантаження…</iframe
+>
+
+<style>
+	iframe {
+		width: 100%;
+		height: 100vh;
+	}
+</style>
